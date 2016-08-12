1f061f06ecfdf0d24bff1995931afaa0388699fa
David Briggs <godamonra@gmail.com>
2016-08-11 21:40:53 -0400

    add system name to page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Anoikis"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} ∙ #{base_title}"
+    end
+  end
 end
