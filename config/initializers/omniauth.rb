e41cc2b2f48bc234bdcce94681c4a96d57c209a4
David Briggs <godamonra@gmail.com>
2016-11-09 00:09:03 -0500

    add new scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :eve_sso, ENV['SSO_ID'], ENV['SSO_SECRET'],
-    scope: 'characterLocationRead characterNavigationWrite remoteClientUI'
+    scope: 'characterNavigationWrite esi-location.read_location.v1 esi-location.read_ship_type.v1'
 end
